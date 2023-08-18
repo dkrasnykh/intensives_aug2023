d7b9839d8e42ed2c4f4e8f0a528544cdc43b7c01
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-08-18 19:58:58 +0300

    brace balance

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -2,14 +2,28 @@
 
 package ru.yandex.tasks;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
-        /*
-         * sequence - последовательность скобок []{}() длины до 10^5
-         * Выход: true/false, является ли строка ПСП
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+        for (int i = 0; i < sequence.length(); ++i) {
+            char c = sequence.charAt(i);
+            if (closeToOpen.containsValue(c)) {
+                stack.addLast(c);
+            } else if (stack.isEmpty() || stack.getLast() != closeToOpen.get(c)) {
+                return false;
+            } else {
+                stack.pollLast();
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
@@ -18,5 +32,9 @@ public class Task6BraceBalance {
 
         assert checkBalance(test1);
         assert !checkBalance(test2);
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
