634ae1a02dc3a91c4f3b9bf716fac1b8ed41a6c5
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-08-18 19:42:52 +0300

    bfs

--- src/main/java/ru/yandex/tasks/Task5BFS.java
+++ src/main/java/ru/yandex/tasks/Task5BFS.java
@@ -2,14 +2,25 @@
 
 package ru.yandex.tasks;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task5BFS {
-    public static void runSearch() {
-        /*
-         * Реализация bfs
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+    public static void runSearch(List<Integer> path, int[][] tree, int root) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int v = q.pollFirst();
+            path.add(v);
+            if (tree[v][0] != -1) {
+                q.addLast(tree[v][0]);
+            }
+            if (tree[v][1] != -1) {
+                q.addLast(tree[v][1]);
+            }
+        }
     }
 
     public static int[] getBFSOrder(int[][] tree, int root) {
@@ -19,8 +30,13 @@ public class Task5BFS {
          * tree - двумерный массив, tree[i][0] - номер левого сына, tree[i][1] - номер правого сына (если нет левого / правого сына, соотв. элемент -1).
          * root - корень, откуда нужно начинать обход
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        List<Integer> path = new ArrayList<>();
+        runSearch(path, tree, root);
+        int[] ans = new int[path.size()];
+        for(int i = 0; i < path.size(); ++i){
+            ans[i] = path.get(i);
+        }
+        return ans;
     }
 
     public static void selfCheck() {
@@ -46,5 +62,9 @@ public class Task5BFS {
         assert (Arrays.equals(getBFSOrder(tree, 3), ans2));
         assert (Arrays.equals(getBFSOrder(tree, 2), ans3));
         assert (Arrays.equals(getBFSOrder(tree, 6), ans4));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
