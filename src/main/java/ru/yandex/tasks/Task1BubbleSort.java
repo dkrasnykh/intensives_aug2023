700a3d327f8c282e1c4b8ac26d6e6476577bf154
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-08-18 18:22:16 +0300

    task 1 bubble sort

--- src/main/java/ru/yandex/tasks/Task1BubbleSort.java
+++ src/main/java/ru/yandex/tasks/Task1BubbleSort.java
@@ -7,12 +7,16 @@ import java.util.Arrays;
 
 public class Task1BubbleSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
-        /*
-         * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
-         * Выход: отсортированный (сортировкой пузырьком!) numbers
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size() - 2; ++i) {
+            for(int j = 0; j < numbers.size() - i - 2; ++j){
+                if(numbers.get(j) > numbers.get(j + 1)){
+                    int tmp = numbers.get(j);
+                    numbers.set(j, numbers.get(j + 1));
+                    numbers.set(j + 1, tmp);
+                }
+            }
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
@@ -20,5 +24,9 @@ public class Task1BubbleSort {
         ArrayList<Integer> output = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
         assert output.equals(sort(input));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
