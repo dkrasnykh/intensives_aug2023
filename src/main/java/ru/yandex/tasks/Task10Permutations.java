128a79509c2966cc461c0c2349f26b48ae7e7727
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-08-18 21:45:33 +0300

    task 10

--- src/main/java/ru/yandex/tasks/Task10Permutations.java
+++ src/main/java/ru/yandex/tasks/Task10Permutations.java
@@ -1,14 +1,48 @@
 package ru.yandex.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task10Permutations {
+    private static List<Integer> permutation;
+
+    public static void generate(boolean[] chosen, List<List<Integer>> ans, int[] numbers) {
+        if (permutation.size() == numbers.length) {
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < numbers.length; ++i){
+                list.add(permutation.get(i));
+            }
+            ans.add(list);
+        } else {
+            for(int i = 0; i < numbers.length; ++i){
+                if(chosen[i]){
+                    continue;
+                }
+                chosen[i] = true;
+                permutation.add(numbers[i]);
+                generate(chosen, ans, numbers);
+                chosen[i] = false;
+                permutation.remove(permutation.size() - 1);
+            }
+        }
+    }
+
     public static int[][] permutations(int[] numbers) {
         /*
          * Возвращает массив со всеми перестановками массива numbers (массив n чисел от 1 до n, 1 <= n <= 10)
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        permutation = new ArrayList<>();
+        boolean[] chosen = new boolean[numbers.length + 1];
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(chosen, ans, numbers);
+        int[][] result = new int[ans.size()][numbers.length];
+        for(int i = 0; i < ans.size(); ++i){
+            for(int j = 0; j < numbers.length; ++j){
+                result[i][j] = ans.get(i).get(j);
+            }
+        }
+        return result;
     }
 
     public static void selfCheck() {
@@ -23,5 +57,9 @@ public class Task10Permutations {
         };
 
         assert Arrays.deepEquals(output, permutations(input));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
